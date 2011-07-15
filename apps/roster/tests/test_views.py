4c29d82732b510c0eb5181951496fccd4885ca12
Peter Bengtsson <mail@peterbe.com>
2011-07-15 17:05:41 +0100

    inserted tri-licence MPL into every source file

--- apps/roster/tests/test_views.py
+++ apps/roster/tests/test_views.py
@@ -1,3 +1,38 @@
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is Mozilla Sheriff Duty.
+#
+# The Initial Developer of the Original Code is Mozilla Corporation.
+# Portions created by the Initial Developer are Copyright (C) 2011
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+
 import re
 from urlparse import urlparse
 import datetime
@@ -297,6 +332,7 @@ class RosterTest(TestCase):
         ok_('offer' in email.subject.lower())
         ok_('swap' in email.subject.lower())
         eq_(email.from_email, settings.SEND_EMAIL_FROM)
+        eq_(email.body.lstrip(), email.body)
         ok_(slot.date.strftime(settings.DEFAULT_DATE_FORMAT) in email.body)
         ok_(peter.first_name in email.body)
         ok_(admin.username in email.body)
@@ -330,6 +366,7 @@ class RosterTest(TestCase):
         email = mail.outbox[-1]
         eq_(email.from_email, settings.SEND_EMAIL_FROM)
         eq_(email.to, [peter.email])
+        eq_(email.body.lstrip(), email.body)
         ok_('accepted' in email.body.lower())
         ok_('offer' in email.body.lower())
         ok_(settings.EMAIL_SIGNATURE.strip() in email.body)
--- apps/vcal/models.py
+++ apps/vcal/models.py
@@ -1,3 +1,1 @@
-from django.db import models
-
-# Create your models here.
+# exists so Django thinks this is an app
